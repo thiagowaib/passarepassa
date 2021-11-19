475ae4bf97d99f7f0690243244189d513d9dc186
thiagowaib <thiagowaib@gmail.com>
2021-11-19 02:48:28 -0300

    [fix] Rotas das Samples

--- scripts/press.js
+++ scripts/press.js
@@ -1,5 +1,5 @@
-const audioAzul = new Audio('../samples/sound_azul.mp3')
-const audioVermelho = new Audio('../samples/sound_vermelho.mp3')
+const audioAzul = new Audio('./../samples/sound_azul.mp3')
+const audioVermelho = new Audio('./../samples/sound_vermelho.mp3')
 
 function handleButtonPress(e){
     if(e.classList.value==="vermelho")
